389da1d5583066528f1d69983b4453aa1c6bd62c
vaibhavchandra13 <82270230+vaibhavchandra13@users.noreply.github.com>
2021-08-07 20:00:51 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,52 @@
+# camilaMusic Telegram Bot - This is a pyrogram bot based on pytgcalls for playing songs or audio files in Telegram group voice chat
+[![camila](https://telegra.ph/file/87f281fe90bb91d3d6193.jpg)](https://t.me/camila_support)
+
+
+# How To Host
+The easiest way to deploy this Bot
+<p align="center"><a href="https://heroku.com/deploy?template=https://github.com/war-legend/camilaVcBot"> <img src="https://img.shields.io/badge/Deploy%20To%20Heroku-red?style=for-the-badge&logo=heroku" width="220" height="38.45"/></a></p>
+
+Get STRING_NAME from here:
+
+[![GenerateString](https://img.shields.io/badge/repl.it-generateString-bluegreen)](https://t.me/PyroStringSessionRobot)
+
+### Mandatory Vars.
+
+- Some Of The Mandatory Vars Are :-
+   - `API_ID` :  Give API_ID of your Alternate Telegram Account. also get from here [Here](https://t.me/MyTelegramOrg_RoBot )
+   - `API_HASH` :  Give API_HASH of your Alternate Telegram Account. also get from here [Here](https://t.me/MyTelegramOrg_RoBot)
+   - `STRING_NAME` :  Make a string session from [here](https://t.me/PyroStringSessionRobot)
+   - `BOT_TOKEN` :  Make a Bot from [@Botfather](https://t.me/botfather) and fill it's bot token.
+   - `SUDO_USERS` :  Fill Userid of yhe users whom you want to be able to control the bot. You can add multiple id by giving a space in b/w each id.
+
+
+
+
+
+
+
+## Commands 🛠
+
+- `/play <song name>` - play song you requested
+- `/dplay <song name>` - play song you requested via deezer
+- `/splay <song name>` - play song you requested via jio saavn
+- `/playlist` - Show now playing list
+- `/current` - Show now playing
+- `/song <song name>` - download songs you want quickly
+- `/search <query>` - search videos on youtube with details
+- `/deezer <song name>` - download songs you want quickly via deezer
+- `/saavn <song name>` - download songs you want quickly via saavn
+- `/video <song name>` - download videos you want quickly
+
+#### Admins only.
+- `/player` - open music player settings panel
+- `/pause` - pause song play
+- `/resume` - resume song play
+- `/skip` - play next song
+- `/end` - stop music play
+- `/userbotjoin` - invite assistant to your chat
+- `/userbotleave` - remove assistant from your chat
+- `/admincache` - Refresh admin list
+
+#[Need Help Join support](https://t.me/warbotZsupport)
+
---
+++ handlers/start.py
@@ -0,0 +1,113 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+
+@Client.on_message(
+    filters.command("start")
+    & filters.private
+    & ~ filters.edited
+)
+async def start_(client: Client, message: Message):
+    await message.reply_sticker("CAACAgIAAxkBAAELMf1hDpeveh_y4U-XDWAwPYiGHSZQogACrgIAAjZ2IA54Lv6p2p7e4yAE")
+    await message.reply_text(
+        f"""<b>Hi {message.from_user.first_name}!
+\nI can play music in your group's voice chat
+
+Music Assistant - @YooAssistant
+\nTo add in your group contact us at @YooSupport or do /userbotjoin
+\nHit /help list of available commands.
+ </b>""",
+      
+       
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                      "✨Assistant", url="https://t.me/YooAssistant",
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "💬 Help Group", url="https://t.me/yoosupport"
+                    ),
+                    InlineKeyboardButton(
+                        "Owner💖", url="https://t.me/ItzMeYoo"
+                    ),
+                    InlineKeyboardButton(
+                        "✨GitHub✨", url="http://www.github.com/War-Legend/camilavcbot"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "➕ Add To Your Group ➕", url="https://t.me/YooMusicBot?startgroup=true"
+                    ) 
+                ]
+            ]
+        )
+    )
+
+@Client.on_message(
+    filters.command("start")
+    & filters.group
+    & ~ filters.edited
+)
+async def start(client: Client, message: Message):
+    await message.reply_text(
+        "💁🏻‍♂️ Do you want to search for a YouTube video?",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Support Group  ", url="https://t.me/yoosupport"
+                    )
+                ],    
+                [    
+                    InlineKeyboardButton(
+                        "✅ Yes", switch_inline_query_current_chat=""
+                    ),
+                    InlineKeyboardButton(
+                        "No ❌", callback_data="close"
+                    )
+                ]
+            ]
+        )
+    )
+
+@Client.on_message(
+    filters.command("help")
+    & filters.private
+    & ~ filters.edited
+)
+async def help(client: Client, message: Message):
+    await message.reply_text(
+        f"""<b>Hi {message.from_user.first_name}!
+\n/play <song name> - play song you requested
+/dplay <song name> - play song you requested via deezer
+/splay <song name> - play song you requested via jio saavn
+/playlist - Show now playing list
+/current - Show now playing
+/song <song name> - download songs you want quickly
+/search <query> - search videos on youtube with details
+/deezer <song name> - download songs you want quickly via deezer
+/saavn <song name> - download songs you want quickly via saavn
+/video <song name> - download videos you want quickly
+\n*Admins only*
+/player - open music player settings panel
+/pause - pause song play
+/resume - resume song play
+/skip - play next song
+/end - stop music play
+/userbotjoin - invite assistant to your chat
+/admincache - Refresh admin list
+ </b>""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Need Help❓", url="https://t.me/YooSupport"
+                    )
+                ]
+            ]
+        )
+    )    
